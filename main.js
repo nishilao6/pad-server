672db2850b3e6a9adbe1a633c96a80a966487d89
庸医治大病 <1683280363@qq.com>
2024-09-18 19:49:36 +0800

    jj

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const net = require("net")
 const { getFASHEDANYUAN } = require("./router/fashedanyuan");
 const { getMUBIAO } = require("./router/mubiaodanyuan");
 const { getDANDAO } = require("./router/dandao");
-const hi='hi'
+
 
 // 发射单元文件
 let fashedanyuanFilePath = "./mock/fashedanyuan.csv";
@@ -22,7 +22,7 @@ let fileList = fs.readdirSync("./mock", (err) => {
 // 读取发射单元文件
 let fileCont = fs.readFileSync(fashedanyuanFilePath, "utf-8");
 
-// 获取发射单元
+// 获取发射单元 s s
 app.get("/fashedanyuan", async function (req, res) {
 	let result = await getFASHEDANYUAN(fashedanyuanFilePath);
 	res.send(result);
